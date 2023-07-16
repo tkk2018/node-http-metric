1521b8519ad3f796f9a305142822d3da2b4b01d0
tkk2018 <tkuankiat@hotmail.com>
2023-07-16 10:28:14 +0800

    First commit.

---
+++ index.ts
@@ -0,0 +1,32 @@
+"use strict";
+
+export interface HttpMetric {
+  method?: string;
+  url?: string;
+};
+
+export interface RequestMetric {
+  start_time: number;
+  headers: Record<string, any>;
+  data?: any; // request body if has
+};
+
+export interface EndMetric {
+  end_time: number;
+  response_time: number;
+  request?: RequestMetric;
+  headers?: Record<string, any>; // not sure whether error will change the headers, play defensive here.
+}
+
+export interface ResponseMetric extends EndMetric {
+  response: {
+    // headers?: Record<string, any>;
+    status_code: number,
+    status_message: string,
+    data?: any
+  };
+};
+
+export interface ErrorMetric extends EndMetric {
+  error: unknown;
+};
